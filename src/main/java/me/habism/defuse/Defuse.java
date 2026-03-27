d569d93fb323ca0dbd5b7e17f053b8ca9392f0fb
Habis Muhammed <habismuh@yahoo.com>
2026-03-27 21:55:17 +0530

    First commit
    init commit

---
+++ src/main/java/me/habism/defuse/Defuse.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.habism.defuse;
+
+/**
+ *
+ * @author habism
+ */
+public class Defuse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //TODO: Read file.
+    }
+}
