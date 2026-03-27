be494759890c916533aeba0fda115e36ec3f4387
Habis Muhammed <habismuh@yahoo.com>
2026-03-27 22:08:14 +0530

    Commit before thunderstorms

--- src/main/java/me/habism/defuse/Defuse.java
+++ src/main/java/me/habism/defuse/Defuse.java
@@ -9,9 +9,11 @@ package me.habism.defuse;
  * @author habism
  */
 public class Defuse {
-
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        //TODO: Read file.
+        var file = "/map.txt";
     }
+    
+    //TODO: logger wrapper class.
 }
